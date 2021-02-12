5590a1a4c56396994fff9f52b11e288801dc785c
marcosRaphael <marcos_8844@hotmail.com>
2021-02-11 23:48:27 -0300

    Department insert, update, delete created.

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -3,16 +3,28 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
+	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
-		
-		return list;
+		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
 	}
 }
